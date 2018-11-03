a2ab0cb4ffa56ad22be075b487faaf4855afe9a0
ChrisKith <s_kith@u.pacific.edu>
2018-11-03 13:35:01 -0700

    Imported some tools for the classes. Also created the key strokes.

--- src/game/game/Bird.java
+++ src/game/game/Bird.java
@@ -1,11 +1,15 @@
 package game;
 
-
+/*
+ * The class bird will be in charge of how the bird will behave.
+ */
 public class Bird {
 	
 	public float x, y, vx, vy;
 	
 	public static final int RAD = 25;
+	//private Physics p;
+	
 	
 	
 	public Bird() {
@@ -14,6 +18,24 @@ public class Bird {
 		y = ConsoleGame.HEIGHT/2;
 		
 	}
+	
+	public void jump() {
+		vy = -8;
+	}
+	
+	public void physics() {
+		x += vx;
+		y += vy;
+		vy += 0.5f;
+		//p.setX(vx);
+	}
+	//Method will reset the bird's location
+	public void reset() {
+		x = 640/2;
+		y = 640/2;
+		vx = vy = 0;
+	}
+	
 	
 	
 
--- src/game/game/ConsoleGame.java
+++ src/game/game/ConsoleGame.java
@@ -1,11 +1,18 @@
 package game;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
-public class ConsoleGame {
+public class ConsoleGame implements ActionListener, KeyListener{
 	
 	public static final int FPS = 60;
 	public static final int WIDTH = 640;
 	public static final int HEIGHT = 480;
+	
+	private Bird b;
 
 	
 	
@@ -13,5 +20,38 @@ public class ConsoleGame {
 		// TODO Auto-generated constructor stub
 		
 	}
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
