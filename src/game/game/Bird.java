b1af2be12fc7bff65fe4fdeef2ade9c0f7b17706
Dharak Vasavda <d_vasavda@u.pacific.edu>
2018-11-05 00:34:53 -0800

    Added tons of documentation, TODOs, and fixed up ambiguous code & logic.

--- src/game/game/Bird.java
+++ src/game/game/Bird.java
@@ -1,89 +1,93 @@
 package game;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.Graphics;
-
 
 /*
- * The class bird will be in charge of how the bird will behave.
- * TODO
- * -Implement the physics class
- * 
+ * The Bird class will be in charge of the bird's behavior
  */
-public class Bird 
-{
+public class Bird {
 	
 	public float x, y;
-	public float vx, vy;
+	public float xspeed, yspeed;
 	private Physics p;
 	
+	// Height and Width are to model the iPhone 6 Plus, rounded up
+	private static final int WINDOW_HEIGHT = 420;
+	private static final int WINDOW_WIDTH= 740;
 	
-	public Bird()
+	
+	public Bird() 
 	{
 		x = ConsoleGame.WIDTH/2;
 		y = ConsoleGame.HEIGHT/2;
 		
 	}
 	
-	//Method will make the bird fly/jump. 
-	public void jump() 
+	/* birdJump() will make the bird fly/jump. 
+	 * All that should be needed here is an downwards
+	 * motion, hence yspeed iteration down. The bird will
+	 * start falling down as gravity acts upon it
+	 */
+	public void birdJump() 
 	{
-		vy += -8;
+		yspeed += 0;
 	}
 	
-	//Can use a separate physics class to handle physics.
-	public void physics() 
+	/*
+	 * The plan of attack in birdPhysics() is to simply create
+	 * and use two instance variables, x/y, xspeed/yspeed, which
+	 * both interact to determine initial location which then
+	 * is added by the xspeed and yspeed multiplier
+	 */
+	
+	// TODO: Implement PVector in birdPhysics() instead of integers
+	// https://processing.org/tutorials/pvector/
+	public void birdPhysics() 
 	{
-		x += vx;
-		y += vy;
-		vy += 0.5f;
-		/*float x = p.getX();
-		float y = p.getY();
-		x += vx;
-		y += vy;*/
+		// Original position is incremented by speed multiplier
+		x += xspeed;
+		y += yspeed;
 		
+		/* TODO: Test out the y-speed below, which
+		 * will act as the downward gravity upon the bird
+		 */
+		yspeed += 0;
 	}
 	
-	//Method will reset the bird's location
-	public void reset() 
+	// resetBirdLocation() will reset the bird's location to original
+	public void resetBirdLocation() 
 	{
-		x = 640/2;
-		y = 480/2;
-		vx = vy = 0;
+		x = WINDOW_HEIGHT/2;
+		y = WINDOW_WIDTH/2;
+		xspeed = yspeed = 0;
 	}
 	
-	//add the bird to 0,0 location. 
+	
+	/* TODO: Add the bird to 0,0 location,
+	 * we may need to think about using an 
+	 * instance variable here
+	 */
 	public void draw(Graphics temp)
 	{
 		ImageIcon birdPic = new ImageIcon("path-location");
-		temp.drawImage(birdPic.getImage(), 0, 0, null);
+
+		// TODO: Get Bird picture
+		//temp.drawImage(birdPic.getImage(), 0, 0, null);
 	}
 	
-	public void setX(float x)
-	{
-		this.x = x;
-	}
-	
-	public void setY(float y)
-	{
-		this.y = y;
-	}
-	
-	public float getX()
-	{
+	public float getX() {
 		return x;
 	}
 	
-	public float getY()
-	{
+	public float getY() {
 		return y;
 	}
 	
-
-
+	public void setX(float x) {
+		this.x = x;
+	}
 	
-	
-	
-
+	public void setY(float y) {
+		this.y = y;
+	}
 }
