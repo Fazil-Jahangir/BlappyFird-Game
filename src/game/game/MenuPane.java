d2ec9bbd4d5ea8c36d486316dfc3945f92f016f9
ChrisKith <s_kith@u.pacific.edu>
2018-11-17 23:59:49 -0800

    Added a Menu pane. Currently not functioning yet, but just worked on design.
    MainApplication will be the main controller, currently linked with
    MenuPane. Running it will just show the menu.

---
+++ src/game/game/MenuPane.java
@@ -0,0 +1,69 @@
+package game;
+
+import starter.GButton;
+import starter.GraphicsPane;
+import java.awt.Color;
+import java.util.ArrayList;
+
+
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+
+public class MenuPane extends GraphicsPane {
+	
+	private MainApplication program;
+	
+	private GButton newGame = new GButton("Start Game", (program.WINDOW_WIDTH/2)-58, 200, MainApplication.BUTTON_HEIGHT,
+			MainApplication.BUTTON_WIDTH, Color.green);
+	private GRect menuBox = new GRect(390, 50, 250, 500);
+	private GLabel titleLabel = new GLabel("Blappy Fird", 397, 87.5);
+	private GLabel menuLabel = new GLabel("MAIN MENU", 410, 166.6);
+	
+	private String[] gbuttonStrings = { "Instructions", "Settings", "High Scores", "Quit" };
+	private ArrayList<GButton> gButtons = new ArrayList<GButton>();
+	
+	public MenuPane(MainApplication app) {
+		program = app;
+		for (int i = 0; i <= 3; i++) {
+			GButton button = new GButton(gbuttonStrings[i], (program.WINDOW_WIDTH/2)-58, 270 + 70 * i, program.BUTTON_HEIGHT,
+					program.BUTTON_WIDTH);
+			button.setFillColor(Color.GREEN);
+			gButtons.add(button);
+			
+		}
+		titleLabel.setColor(Color.WHITE);
+		menuLabel.setColor(Color.WHITE);
+		titleLabel.setFont(MainApplication.titleFont);
+		menuLabel.setFont(MainApplication.menuFont);
+		menuBox.setFillColor(Color.BLACK);
+		menuBox.setFilled(true);
+		showContents();
+	}
+	
+	@Override
+	public void showContents() {
+		program.add(MainApplication.background);
+		program.add(menuBox);
+		program.add(newGame);
+		program.add(titleLabel);
+		program.add(menuLabel);
+		for (GButton button : gButtons) {
+			program.add(button);
+		}
+		
+	}
+
+	@Override
+	public void hideContents() {
+		program.remove(menuBox);
+		program.remove(newGame);
+		program.remove(titleLabel);
+		program.remove(menuLabel);
+		for (GButton button : gButtons) {
+			program.remove(button);
+		}
+		
+	}
+	
+}
