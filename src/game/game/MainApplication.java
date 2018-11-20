3ba1e0f55ceb1167ca8485f618f300a9c878bad9
Fazil <luvfazil@gmail.com>
2018-11-19 22:49:05 -0800

    Created Menu System but needs tweaking!

--- src/game/game/MainApplication.java
+++ src/game/game/MainApplication.java
@@ -1,24 +1,20 @@
 package game;
 
-import java.awt.Font;
-
+import java.awt.Color;
 import acm.graphics.GImage;
-import starter.GraphicsApplication;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
 
-public class MainApplication extends GraphicsApplication 
+public class MainApplication extends GraphicsApplication
 {
-	public static final int WINDOW_HEIGHT = 576;
-	public static final int WINDOW_WIDTH = 1024;
+	public static final int WINDOW_HEIGHT = 535;
+	public static final int WINDOW_WIDTH = 1000;
 	public static final int BUTTON_HEIGHT = 120;
 	public static final int BUTTON_WIDTH = 50;
 	
-	private MenuPane menu;
+	private MenuPane menuPane;
 	
-	public static GImage background = new GImage("background.png", 0, 0);
-	public static Font titleFont = new Font("Serif", Font.BOLD, 46);
-	public static Font menuFont = new Font("Serif", Font.BOLD, 36);
-	
-
+	public static GImage background = new GImage("menu3.png", 0, 0);
 	
 	public void init() 
 	{
@@ -27,6 +23,22 @@ public class MainApplication extends GraphicsApplication
 	
 	public void run() 
 	{
-		menu = new MenuPane(this);
+		menuPane = new MenuPane(this);
+		switchToScreen(menuPane);
 	}
+	
+	/*
+	public static void highlightsButton(GButton button, GObject obj, int r, int g, int b) 
+	{
+		Color highlightColor = new Color(r, g, b);
+		if (obj == button)
+		{
+			button.setFillColor(highlightColor);
+		}			
+		else 
+		{
+			button.setFillColor(Color.BLACK);
+		}
+	}
+	*/
 }
--- src/game/game/MenuPane.java
+++ src/game/game/MenuPane.java
@@ -1,68 +1,110 @@
 package game;
 
-import starter.GButton;
-import starter.GraphicsPane;
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GRect;
 
 public class MenuPane extends GraphicsPane 
-{
-	
+{	
 	private MainApplication program;
 	
-	private GButton newGame = new GButton("Start Game", (MainApplication.WINDOW_WIDTH/2)-58, 200, MainApplication.BUTTON_HEIGHT, MainApplication.BUTTON_WIDTH, Color.green);
-	private GRect menuBox = new GRect(390, 50, 250, 500);
-	private GLabel titleLabel = new GLabel("Blappy Fird", 397, 87.5);
-	private GLabel menuLabel = new GLabel("MAIN MENU", 410, 166.6);
+	private static GRect menuBox = new GRect(0, 150, 1000, 60);
 	
-	private String[] gbuttonStrings = { "Instructions", "Settings", "High Scores", "Quit" };
+	private String[] gbuttonStrings = { "Start Game", "Instructions", "Settings", "High Scores", "Quit" };
 	private ArrayList<GButton> gButtons = new ArrayList<GButton>();
 	
 	public MenuPane(MainApplication app) 
 	{
 		program = app;
-		for (int i = 0; i <= 3; i++) 
+		for (int i = 0; i < 5; i++) 
 		{
-			GButton button = new GButton(gbuttonStrings[i], (MainApplication.WINDOW_WIDTH/2)-58, 270 + 70 * i, MainApplication.BUTTON_HEIGHT, MainApplication.BUTTON_WIDTH);
-			button.setFillColor(Color.GREEN);
-			gButtons.add(button);
-			
+			GButton button = new GButton(gbuttonStrings[i], (MainApplication.WINDOW_WIDTH/2)-230, 160 + 70 * i, MainApplication.BUTTON_HEIGHT, MainApplication.BUTTON_WIDTH);
+			button.setColor(Color.WHITE);
+			button.setFilled(true);
+			button.setCustomFont();
+			gButtons.add(button);			
 		}
-		titleLabel.setColor(Color.WHITE);
-		menuLabel.setColor(Color.WHITE);
-		titleLabel.setFont(MainApplication.titleFont);
-		menuLabel.setFont(MainApplication.menuFont);
-		menuBox.setFillColor(Color.BLACK);
+		menuBox.setFillColor(Color.RED);
 		menuBox.setFilled(true);
-		showContents();
+		menuBox.setVisible(false);
 	}
 	
 	@Override
-	public void showContents() {
+	public void showContents() 
+	{
 		program.add(MainApplication.background);
 		program.add(menuBox);
-		program.add(newGame);
-		program.add(titleLabel);
-		program.add(menuLabel);
-		for (GButton button : gButtons) {
+		for (GButton button : gButtons)
+		{
 			program.add(button);
 		}
 		
 	}
 
 	@Override
-	public void hideContents() {
+	public void hideContents() 
+	{
+		program.remove(MainApplication.background);
 		program.remove(menuBox);
-		program.remove(newGame);
-		program.remove(titleLabel);
-		program.remove(menuLabel);
-		for (GButton button : gButtons) {
+		for (GButton button : gButtons) 
+		{
 			program.remove(button);
+		}		
+	}	
+	
+	@Override
+	public void mousePressed(MouseEvent e) 
+	{
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == gButtons.get(0)) 
+		{
+			//program.switchToInstructions();
 		}
-		
+		if (obj == gButtons.get(1)) 
+		{
+			//program.switchToSettings();
+		}
+		if (obj == gButtons.get(2)) 
+		{
+			//program.switchToHighScores();
+		}
+		if (obj == gButtons.get(3)) 
+		{
+			//program.switchToCredits();
+		}
+		if (obj == gButtons.get(4)) 
+		{
+			System.exit(0);
+		}
 	}
 	
+	@Override
+	public void mouseMoved(MouseEvent e) 
+	{
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj != null) 
+		{
+			for (GButton button : gButtons) 
+			{
+				highlightsButton(button, obj, 255, 255, 255);
+			}
+		}
+	}
+	
+	public static void highlightsButton(GButton button, GObject obj, int r, int g, int b) 
+	{
+		Color highlightColor = new Color(r, g, b);
+		if (obj == button)
+		{
+			menuBox.setVisible(true);
+			menuBox.setLocation(0, button.getY());
+			button.setFillColor(highlightColor);
+		}			
+		else 
+		{
+			button.setFillColor(Color.BLACK);
+		}
+	}
 }
